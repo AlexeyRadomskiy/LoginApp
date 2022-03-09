eb189c17cee572300f71ff130669c75e43a89206
Alexey <alexeyradomskiy98@gmail.com>
2022-03-09 23:58:16 +0300

    ViewController.swift review and keyboard improvement (Return transition)

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -12,40 +12,37 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTextField.text
     }
     
-    @IBAction func logInTappedButton(_ sender: Any) {
-        guard let inputLogin = userNameTextField.text, !inputLogin.isEmpty, userNameTextField.text == "Swift", passwordTextField.text == "qwe" else {
-            showAlert(with: "Invalid login or password", and: "Please, enter correct login and password")
+    @IBAction func logInTappedButton() {
+        guard let inputLogin = userNameTextField.text,
+              !inputLogin.isEmpty,
+              userNameTextField.text == "Swift",
+              passwordTextField.text == "qwe"
+        else {
+            showAlert(
+                with: "Invalid login or password",
+                and: "Please, enter correct login and password"
+            )
             return
         }
     }
     
     @IBAction func forgotUserNameTappedButton() {
-        showAlert(with: "Oops!", and: "Your name is Swift")
+        showAlert(with: "Oops!", and: "Your name is Swift 😉")
+        userNameTextField.text = ""
     }
     
     @IBAction func forgotPasswordTappedButton() {
-        showAlert(with: "Oops!", and: "Your password is qwe")
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let user = segue.destination as? WelcomeViewController else { return }
-        user.userName = userNameTextField.text
+        showAlert(with: "Oops!", and: "Your password is qwe 😉")
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
         userNameTextField.text = ""
         passwordTextField.text = ""
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
-        self.view.endEditing(true)
     }
 }
 
@@ -54,10 +51,27 @@ extension LoginViewController {
     private func showAlert(with title: String, and message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.userNameTextField.text = ""
             self.passwordTextField.text = ""
         }
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInTappedButton()
+            performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
+        }
+        return true
     }
 }
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -13,25 +13,36 @@ class WelcomeViewController: UIViewController {
     
     var userName: String?
     
+    private let firstColor = UIColor(
+        red: 252 / 255,
+        green: 70 / 255,
+        blue: 107 / 255,
+        alpha: 1.0
+    )
+    
+    private let secondColor = UIColor(
+        red: 63 / 255,
+        green: 94 / 255,
+        blue: 251 / 255,
+        alpha: 1.0
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let userName = self.userName else { return }
+        view.setGradientLayer(firstColor: firstColor, secondColor: secondColor)
+        guard let userName = userName else { return }
         welcomeUserLabel.text = "Welcome, \(userName)!"
+    }
+}
 
-        let colorBackgroundOne = UIColor(red: 252 / 255, green: 70 / 255, blue: 107 / 255, alpha: 1.0).cgColor
-        let colorBackgroundTwo = UIColor(red: 63 / 255, green: 94 / 255, blue: 251 / 255, alpha: 1.0).cgColor
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [colorBackgroundOne, colorBackgroundTwo]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-        self.view.layer.insertSublayer(gradientLayer, at: 0)
+// MARK: Set background color
+extension UIView {
+    func setGradientLayer(firstColor: UIColor, secondColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [firstColor.cgColor, secondColor.cgColor]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
-    
-    @IBAction func logOutTappedButton() {
-        dismiss(animated: true)
-    }
-    
 }
